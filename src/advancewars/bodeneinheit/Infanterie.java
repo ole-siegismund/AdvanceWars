7542edb252d08e2f1040ca3fde13efe4ebc175e2
Ole <osiegismund@willigis-online.de>
2023-06-14 11:15:30 +0200

    3L

--- src/advancewars/bodeneinheit/Infanterie.java
+++ src/advancewars/bodeneinheit/Infanterie.java
@@ -1,6 +1,15 @@
 package advancewars.bodeneinheit;
 
+import advancewars.Einheit;
 import advancewars.bodeneinheit.Bodeneinheit;
+import advancewars.bodeneinheit.infanterie.Mech;
+import com.sun.jdi.ClassType;
 
 public class Infanterie extends Bodeneinheit {
+    @Override
+    public void angreifen(Einheit einheit) {
+        double a;
+        if (einheit instanceof Infanterie) a = 0.55;
+        if (einheit instanceof Mech) a = 0.45;
+    }
 }
